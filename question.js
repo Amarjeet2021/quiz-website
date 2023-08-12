bb3bb096da240a49db00d71b90d81a99eb9069b1
Amarjeetkumarbce <127743537+Amarjeet2021@users.noreply.github.com>
2023-08-12 23:41:25 +0530

    Add files via update
    web development quiz website

---
+++ question.js
@@ -0,0 +1,63 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What does HTML stands for?",
+        answer: "D. Hyper text markup language",
+        options: [ 
+            "A. Hyper type multi Language",
+            "B. Hyper type multiple Language",
+            "C. Hype type multilanguage",
+            "D. Hyper text markup language"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What does CSS stand for?",
+        answer: "A. Cascadeing style sheet",
+        options: [
+            "A. Cascadeing style sheet",
+            "B. Colorful Style Sheets",
+            "C. Computer Science Studies",
+            "D. Cute style sheet"
+    
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stands for",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Personal Home Page",
+            "C. Programmer's Hypertext Processor",
+            "D. Puphpet hyper power"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for",
+        answer: "B. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Structured Query Language",
+            "C. Structure Query Language",
+            "D. Science Question Language"
+    
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Extra Markup Language",
+            "C. Explore Multiple Language",
+            "D. Extensible Markup Language"
+            
+
+    
+        ]
+    }
+
+];
